9596d74dc6d8ce68b15fc8aed5314fa06ded5697
Christoph Dorn <christoph@christophdorn.com>
2015-10-06 16:55:44 -0700

    wip

--- for/c9/edit.proto.sh
+++ for/c9/edit.proto.sh
@@ -22,6 +22,60 @@ function init {
 	fi
 
 
+	function ensurePlugin {
+		BO_format "$VERBOSE" "HEADER" "Ensuring Cloud9 plugin ..."
+
+		BO_log "$VERBOSE" "Plugin Name: $1"
+		BO_log "$VERBOSE" "Plugin Path: $2"
+
+		PLUGIN_BASE_PATH=".c9/plugins"
+
+		pushd "$WORKSPACE_DIR" > /dev/null
+			if [ ! -e "$PLUGIN_BASE_PATH" ]; then
+				mkdir -p "$PLUGIN_BASE_PATH"
+			fi
+			BO_log "$VERBOSE" "Linking plugin from '$2' to runtime location '$PLUGIN_BASE_PATH/$1'"
+
+			rm -Rf "$PLUGIN_BASE_PATH/$1" > /dev/null || true
+			ln -s "$2" "$PLUGIN_BASE_PATH/$1"
+
+			# TODO: Remove once implemented: https://github.com/c9/core/issues/172
+			if [ ! -e "$HOME/$PLUGIN_BASE_PATH" ]; then
+				mkdir -p "$HOME/$PLUGIN_BASE_PATH"
+			fi
+			rm -Rf "$HOME/$PLUGIN_BASE_PATH/$1" > /dev/null || true
+			ln -s "$2" "$HOME/$PLUGIN_BASE_PATH/$1"
+
+		popd > /dev/null
+
+		BO_format "$VERBOSE" "FOOTER"
+	}
+	
+	function ensurePluginSetting {
+		BO_format "$VERBOSE" "HEADER" "Ensuring Cloud9 plugin setting ..."
+
+		BO_log "$VERBOSE" "Plugin Name: $1"
+		BO_log "$VERBOSE" "Settings key: $2"
+		BO_log "$VERBOSE" "Settings value: $2"
+
+		SETTINGS_PATH=".c9/project.settings"
+
+		node --eval '
+			const FS = require("fs");
+			var config = JSON.parse(FS.readFileSync("'$SETTINGS_PATH'"));
+			var pluginName = "'$1'";
+			var key = "'$2'";
+			var value = "'$3'";
+			if (!config[pluginName]) {
+				config[pluginName] = {};
+			}
+			config[pluginName][key] = value;
+			FS.writeFileSync("'$SETTINGS_PATH'", JSON.stringify(config, null, 4));
+		'
+
+		BO_format "$VERBOSE" "FOOTER"
+	}
+
 	function launchEditor {
 		BO_format "$VERBOSE" "HEADER" "Ensuring and launching Cloud9"
 
